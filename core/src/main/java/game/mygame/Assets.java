767d2645d66070e40e5040522bc9c2dce8fb079f
NurdauletZhussip-tech <nurdaulet.zhussip@narxoz.kz>
2026-05-17 19:23:33 +0500

    feat(assets): add boss texture loading
    - Add new static Texture field for bossTexture
    - Load boss texture from "sprites/enemies/boss.png"
    - Update load() method to include boss texture

--- core/src/main/java/game/mygame/Assets.java
+++ core/src/main/java/game/mygame/Assets.java
@@ -14,7 +14,7 @@ public class Assets {
     public static Texture enemyBlack;
     public static Texture enemyGreen;
     public static Texture explosion;
-
+    public static Texture bossTexture;
     public static void load() {
 
         manager.load("sprites/player/PlayerShip.png", Texture.class);
@@ -24,7 +24,7 @@ public class Assets {
         manager.load("sprites/enemies/enemy_fighter.png", Texture.class);
         manager.load("sprites/enemies/enemy_scout.png", Texture.class);
         manager.load("sprites/effects/explosion_1.png", Texture.class);
-
+        manager.load("sprites/enemies/boss.png", Texture.class);
         manager.finishLoading();
 
         playerShip = manager.get("sprites/player/PlayerShip.png", Texture.class);
@@ -34,6 +34,7 @@ public class Assets {
         enemyBlack = manager.get("sprites/enemies/enemy_fighter.png", Texture.class);
         enemyGreen = manager.get("sprites/enemies/enemy_scout.png", Texture.class);
         explosion = manager.get("sprites/effects/explosion_1.png", Texture.class);
+        bossTexture = manager.get("sprites/enemies/boss.png", Texture.class);
     }
 
     public static void dispose() {
